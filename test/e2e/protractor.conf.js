0fbe2e45f4135f1d29e50fe11a5c1cec5ba10b2d
Chenfu Wang <chenfu@localhost.localdomain>
2017-08-04 14:36:41 +0100

    :art: re-organize the e2e tests into test/e2e and never comes out

---
+++ test/e2e/protractor.conf.js
@@ -0,0 +1,38 @@
+'use strict';
+const path = require('path');
+
+exports.config = sharedConfig();
+
+function sharedConfig() {
+    const config = {
+        framework: 'custom',
+        frameworkPath: require.resolve('protractor-cucumber-framework'),
+        cucumberOpts: {
+            compiler: "ts:ts-node/register",
+            require: [
+                path.resolve(process.cwd(), './tmp/*.ts')
+            ],
+            format: 'pretty',
+            tags: ''
+        },
+		capabilities: {
+			'browserName': 'chrome'
+		},
+        specs: ['features/*.feature'],
+
+        onPrepare: function () {
+            // place something here
+        },
+		plugins: [{
+			package: require.resolve('aurelia-protractor-plugin')
+		}],
+
+        allScriptsTimeout: 11000,
+        disableChecks: true,
+
+        // From `protractor-cucumber-framework`, allows cucumber to handle the 199 exception and record it appropriately
+        ignoreUncaughtExceptions: true
+    };
+
+    return config;
+}
