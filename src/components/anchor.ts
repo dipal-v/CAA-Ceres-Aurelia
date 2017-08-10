2232c4cd0cb48870e6d27841149bfecdd6840459
Chenfu Wang <chenfu.wang@inmarsat.com>
2017-08-10 11:40:35 +0100

    :art: update tabs

--- src/components/anchor.ts
+++ src/components/anchor.ts
@@ -4,12 +4,12 @@ import {customElement, bindable, inject, bindingMode} from 'aurelia-framework';
 @bindable({ name: 'cool', attribute: 'data', defaultBindingMode: bindingMode.oneWay})
 @inject(Element)
 export class Status {
-	constructor(private element: Element) {
-		this.element = element;
-	}
+    constructor(private element: Element) {
+	this.element = element;
+    }
 
-	public click(event){
-		let e = new CustomEvent('single', {bubbles: true});
-		this.element.dispatchEvent(e);
-	}
+    public click(event){
+	let e = new CustomEvent('single', {bubbles: true});
+	this.element.dispatchEvent(e);
+    }
 }
