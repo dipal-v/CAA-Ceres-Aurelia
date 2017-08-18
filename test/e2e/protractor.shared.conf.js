cb90520f2d2c47e10c4f5560db567ae83a10a9f8
Chenfu Wang <chenfu.wang@inmarsat.com>
2017-08-18 17:12:25 +0100

    :sparkles: first ever e2e integration of cumcumber, webpack, protractor, typescript, aurelia

--- test/e2e/protractor.shared.conf.js
+++ test/e2e/protractor.shared.conf.js
@@ -10,22 +10,43 @@ function sharedConfig() {
         cucumberOpts: {
             compiler: "ts:ts-node/register",
             require: [
-                path.resolve(process.cwd(), './src/*.ts')
+                path.resolve(process.cwd(), './test/e2e/src/*.ts')
             ],
             format: 'pretty',
             tags: ''
         },
-	capabilities: {
-	    'browserName': 'chrome'
-	},
+        capabilities: {
+            'browserName': 'chrome',
+            'chromeOptions': {
+                'args': [
+                    '--show-fps-counter',
+                    '--no-default-browser-check',
+                    '--no-first-run',
+                    '--disable-default-apps',
+                    '--disable-popup-blocking',
+                    '--disable-translate',
+                    '--disable-background-timer-throttling',
+                    '--disable-renderer-backgrounding',
+                    '--disable-device-discovery-notifications',
+                    /* enable these if you'd like to test using Chrome Headless
+                       '--no-gpu',
+                       '--headless'
+                    */
+                ]
+            }
+        },
         specs: ['features/*.feature'],
 
-	plugins: [{
-	    package: require.resolve('aurelia-protractor-plugin')
-	}],
-        
+        plugins: [{
+            package: require.resolve('aurelia-protractor-plugin')
+        }],
+
         allScriptsTimeout: 11000,
         disableChecks: true,
+
+        onPrepare: function() {
+            require('ts-node').register({ compilerOptions: { module: 'commonjs' }, disableWarnings: true, fast: true });
+        },
 
         // From `protractor-cucumber-framework`, allows cucumber to handle the 199 exception and record it appropriately
         ignoreUncaughtExceptions: true
