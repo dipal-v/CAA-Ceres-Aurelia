81a0d6eb953064e8626b748b44ec82194a63fff1
Chenfu Wang <chenfu.wang@inmarsat.com>
2017-08-30 15:28:36 +0100

    use a public aurelia doc for debugging

--- test/e2e/protractor.demo.conf.js
+++ test/e2e/protractor.demo.conf.js
@@ -2,6 +2,7 @@
 
 const config = require('./protractor.shared.conf.js').config;
 
-config.baseUrl = 'http://ppwebtest01.inmarsat.com/caa-ceres-aurelia/';
+//config.baseUrl = 'http://ppwebtest01.inmarsat.com/caa-ceres-aurelia/';
+config.baseUrl = 'http://aurelia.io/hub.html#/doc/persona/developer';
 
 exports.config = config;
